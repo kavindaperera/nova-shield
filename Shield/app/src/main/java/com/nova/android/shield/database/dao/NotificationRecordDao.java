7de02a3fa509623b5e94a639bc655a01aec3a03c
JudeRanidu <juderanidu96@gmail.com>
2022-02-10 11:58:23 +0530

    Add Dao & Model skeleton classes for ContactRecord & NotificationRecord

---
+++ Shield/app/src/main/java/com/nova/android/shield/database/dao/NotificationRecordDao.java
@@ -0,0 +1,4 @@
+package com.nova.android.shield.database.dao;
+
+public interface NotificationRecordDao {
+}
---
+++ Shield/app/src/main/java/com/nova/android/shield/models/NotificationRecord.java
@@ -0,0 +1,4 @@
+package com.nova.android.shield.models;
+
+public class NotificationRecord {
+}
