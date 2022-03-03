8686c6ff6a060e6ebe670e0be8fe2df9d0a6724a
kavindaperera <perera.kavinda52@gmail.com>
2022-03-03 21:59:55 +0530

    Update `NotificationRecord` entity

--- Shield/app/src/main/java/com/nova/android/shield/ui/notification/NotificationRecord.java
+++ Shield/app/src/main/java/com/nova/android/shield/ui/notification/NotificationRecord.java
@@ -1,34 +1,52 @@
 package com.nova.android.shield.ui.notification;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import com.google.gson.Gson;
 
-@Entity(tableName = "notification_record_table", primaryKeys = {"id", "timestamp_start"})
+@Entity(tableName = "notification_record_table")
 public class NotificationRecord {
 
-    private int id; // auto-generated id
+    @PrimaryKey(autoGenerate = true)
+    private int id;
 
+    @NonNull
+    @ColumnInfo(name = "is_new")
     public boolean is_new;
 
+    @NonNull
+    @ColumnInfo(name = "msg")
     public String msg;
 
+    @NonNull
+    @ColumnInfo(name = "notif_type")
     public int notif_type;
 
+    @NonNull
+    @ColumnInfo(name = "timestamp_start")
     public long timestamp_start;
 
+    @NonNull
+    @ColumnInfo(name = "timestamp_end")
     public long timestamp_end;
 
-    public NotificationRecord(long timestamp_start2, long timestamp_end2, String msg2, int notif_type2, boolean is_new2) {
-        this.timestamp_start = timestamp_start2;
-        this.timestamp_end = timestamp_end2;
-        this.msg = msg2;
-        this.notif_type = notif_type2;
-        this.is_new = is_new2;
+    public NotificationRecord(long timestamp_start, long timestamp_end, String msg, int notif_type, boolean is_new) {
+        this.timestamp_start = timestamp_start;
+        this.timestamp_end = timestamp_end;
+        this.msg = msg;
+        this.notif_type = notif_type;
+        this.is_new = is_new;
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public boolean isNew() {
